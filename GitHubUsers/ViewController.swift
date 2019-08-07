ba5ed4ed9f8c568d57aae736c2aec50b55486a4f
Valentin Ozerov <kuliba@gmail.com>
2019-08-07 12:20:31 +0300

    Initial Commit

---
+++ GitHubUsers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubUsers
+//
+//  Created by Valentin Ozerov on 07/08/2019.
+//  Copyright © 2019 Valentin Ozerov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
